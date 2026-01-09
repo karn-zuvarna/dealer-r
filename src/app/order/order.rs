a38f5ecc719c0390ffbcb450492434218b2f30b7
karn-zuvarna <karnake.r@zuvarna.com>
2026-01-09 11:05:11 +0700

    init 20260109

--- src/app/order/order.rs
+++ src/app/order/order.rs
@@ -1,10 +1,16 @@
 use actix_web::{HttpResponse, Responder, web};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct OrderRequest {
-    symbol: String,
+    customer_id: String,
+    asset_id: String,
     quantity: f64,
+    price: Option<f64>,
+    total: Option<f64>,
+    balance: Option<f64>,
+    status: String,
+    order_type: i8,
 }
 
 // 2. นิยามข้อมูลที่จะส่งออก (ใช้ Serialize)
@@ -17,10 +23,10 @@ struct OrderResponse {
 // 3. ฟังก์ชัน Handler สำหรับ POST
 pub async fn create_order(item: web::Json<OrderRequest>) -> impl Responder {
     // เข้าถึงข้อมูลได้ง่ายๆ ผ่าน .into_inner() หรือ .property
-    println!("ได้รับคำสั่งซื้อ: {} จำนวน {}", item.symbol, item.quantity);
+    println!("ได้รับคำสั่งซื้อ: {:?}", item);
 
     HttpResponse::Ok().json(OrderResponse {
         status: "success".into(),
-        message: format!("รับคำสั่งซื้อ {} เรียบร้อยแล้ว", item.symbol),
+        message: format!("รับคำสั่งซื้อ {} เรียบร้อยแล้ว", item.asset_id),
     })
 }
